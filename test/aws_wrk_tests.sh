3044d324c55c12892b5a60933122d2f9ec2b1f03
Joel Bondurant <joel@analyticobjects.com>
2017-05-11 23:51:40 -0700

    wrk test results.

--- test/aws_wrk_tests.sh
+++ test/aws_wrk_tests.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 # Run wrk tests.
 echo 'Starting wrk tests.'
-wrk -c 64 -d 10 -t 32 "http://citysearch:8080/v0/city/hello"
-wrk -c 64 -d 10 -t 32 "http://citysearch:8080/v0/city/count"
-wrk -c 64 -d 10 -t 32 "http://citysearch:8080/v0/city/proximity_search?name=Daly%20City&k=10"
-wrk -c 64 -d 10 -t 32 "http://citysearch:8080/v0/city/text_search?q=San%20Francisco"
+echo ''
+wrk -c 100 -d 10 -t 33 "http://citysearch:8080/v0/city/hello"
+echo ''
+wrk -c 100 -d 10 -t 33 "http://citysearch:8080/v0/city/count"
+echo ''
+wrk -c 100 -d 10 -t 33 "http://citysearch:8080/v0/city/proximity_search?name=Daly%20City&k=10"
+echo ''
+wrk -c 100 -d 10 -t 33 "http://citysearch:8080/v0/city/proximity_search2?name=Daly%20City&k=10"
+echo ''
+wrk -c 100 -d 10 -t 33 "http://citysearch:8080/v0/city/text_search?q=San%20Francisco"
+echo ''
 echo 'Finished wrk tests.'
